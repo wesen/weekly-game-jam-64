a93864e22b54f045c3c7eb8044e76c3230db7d45
Manuel Odendahl <wesen@ruinwesen.com>
2018-09-29 16:25:04 -0400

    Update player script for collision detection

--- weekly-game-jam-64/Assets/Scripts/Player.cs
+++ weekly-game-jam-64/Assets/Scripts/Player.cs
@@ -1,23 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-    private Vector3 moveDelta;
+    private Vector3 _moveDelta;
+    private BoxCollider2D _boxCollider;
+
+    void Awake() {
+        _boxCollider = GetComponent<BoxCollider2D>();
+    }
 
     void FixedUpdate() {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        moveDelta = new Vector3(x, y, 0);
+        _moveDelta = new Vector3(x, y, 0);
 
         // control sprite direction
-        if (moveDelta.x > 0) {
+        if (_moveDelta.x > 0) {
             transform.localScale = Vector3.one;
-        } else if (moveDelta.x < 0) {
+        } else if (_moveDelta.x < 0) {
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        
-        transform.Translate(moveDelta * Time.deltaTime);
+
+        RaycastHit2D hit =
+            Physics2D.BoxCast(transform.position, _boxCollider.size, 0, _moveDelta,
+                _moveDelta.magnitude * Time.deltaTime,
+                LayerMask.GetMask("Collision", "Actors"));
+        if (hit.collider != null) {
+            Debug.Log("hit " + hit.collider + " " + _moveDelta);
+        }
+
+        if (hit.collider == null) {
+            transform.Translate(_moveDelta * Time.deltaTime);
+        }
     }
 }
