152d4c455d4714c50d2927c033eb34e1cbd28bfa
Manuel Odendahl <wesen@ruinwesen.com>
2018-09-28 22:58:07 -0400

    Add movement and sprite direction swapping to player movement

--- weekly-game-jam-64/Assets/Scripts/Player.cs
+++ weekly-game-jam-64/Assets/Scripts/Player.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    private Vector3 moveDelta;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void FixedUpdate() {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        moveDelta = new Vector3(x, y, 0);
+
+        // control sprite direction
+        if (moveDelta.x > 0) {
+            transform.localScale = Vector3.one;
+        } else if (moveDelta.x < 0) {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        
+        transform.Translate(moveDelta * Time.deltaTime);
+    }
 }
