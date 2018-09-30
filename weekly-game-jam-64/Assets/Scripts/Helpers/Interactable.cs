2802646cc97040e67ca97979cfccf23ff0ba34ee
Manuel Odendahl <wesen@ruinwesen.com>
2018-09-30 11:42:09 -0400

    Add tweened floating text on interactables

--- weekly-game-jam-64/Assets/Scripts/Helpers/Interactable.cs
+++ weekly-game-jam-64/Assets/Scripts/Helpers/Interactable.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Interactable : MonoBehaviour {
     private DialogueManager _DialogueManager;
+    private FloatingText _floatingText;
     void Awake() {
         _DialogueManager = FindObjectOfType<DialogueManager>();
+        _floatingText = GetComponentInChildren<FloatingText>();
     }
     
     void OnTriggerEnter2D(Collider2D collider) {
         if (collider.name == "player") {
             _DialogueManager.ShowBox("Some stupid description");
+            _floatingText.ShowText("BEAUTIFUL");
         }
     }
 
