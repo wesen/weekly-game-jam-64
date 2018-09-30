01cb6b206d9ce69799e1afc8c010d28f252032ac
Manuel Odendahl <wesen@ruinwesen.com>
2018-09-30 09:28:00 -0400

    Add PositionLogger script

--- weekly-game-jam-64/Assets/Scripts/Player.cs
+++ weekly-game-jam-64/Assets/Scripts/Player.cs
@@ -9,7 +9,6 @@ public class Player : MonoBehaviour {
     private Animator _animator;
 
     public float Speed = 0.5f;
-    
 
     void Awake() {
         _boxCollider = GetComponent<BoxCollider2D>();
@@ -42,46 +41,4 @@ public class Player : MonoBehaviour {
             _animator.Play("player_idle_frontal");
         }
     }
-
-    //This block will send_player_pos json to server
-    bool isOpen = false;
-    string prevPos = "";
-    Stack posStack = new Stack();
-
-    void OnGUI() {
-        if (isOpen) {
-            if (GUI.Button(new Rect(10, 10, 100, 50), transform.position.ToString())) //Display and use the Yes button
-            {
-                Debug.Log("Yes");
-                isOpen = false;
-            }
-
-            if (GUI.Button(new Rect(120, 10, 100, 50), "Show Stack in Debug")) //Display and use the Yes button
-            {
-                int i = 0;
-                foreach (var pos in posStack) {
-                    Debug.Log(i + ": " + pos);
-                }
-            }
-
-            if (GUI.Button(new Rect(230, 10, 100, 50), "Send Stack to Server")) //Display and use the Yes button
-            {
-                foreach (var pos in posStack) {
-                    //POST
-                }
-            }
-        }
-
-        if (prevPos != transform.position.ToString()) {
-            posStack.Push(prevPos);
-            prevPos = transform.position.ToString();
-        }
-    }
-
-    //Get the mouse click
-    void OnMouseDown() {
-        isOpen = true; //Set the buttons to appear
-    }
-
-    //End send_player_pos block
 }
---
+++ weekly-game-jam-64/Assets/Scripts/PositionLogger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PositionLogger : MonoBehaviour {
+    public float EntryInterval_s = 0.5f;
+    public float LogInterval_s = 10.0f;
+
+    class PositionEntry {
+        public Vector3 Position;
+        public float Time_s;
+
+        public PositionEntry(Vector2 position) {
+            Time_s = Time.time;
+            Position = position;
+        }
+    }
+
+    private List<PositionEntry> _entries = new List<PositionEntry>();
+
+    private float _lastEntryTime_s = 0.0f;
+    private float _lastLogTime_s = 0.0f;
+
+    void Awake() {
+        _lastLogTime_s = Time.time;
+        _addCurrentPosition();
+    }
+
+    void _addCurrentPosition() {
+        _entries.Add(new PositionEntry(transform.position));
+        _lastEntryTime_s = Time.time;
+    }
+
+    void LateUpdate() {
+        if ((Time.time - _lastEntryTime_s) > EntryInterval_s) {
+            _addCurrentPosition();
+        }
+
+        if ((Time.time - _lastLogTime_s) > LogInterval_s) {
+            PositionEntry[] entries = _entries.ToArray();
+            foreach (PositionEntry entry in entries) {
+                Debug.Log("pos " + entry.Position + " at " + entry.Time_s);
+            }
+
+            _lastLogTime_s = Time.time;
+            
+            _entries.Clear();
+            _addCurrentPosition();
+        }
+    }
+}
