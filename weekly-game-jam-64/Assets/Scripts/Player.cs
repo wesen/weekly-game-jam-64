345c8aedc4380094a0858d0b201fd7461798b052
Manuel Odendahl <wesen@ruinwesen.com>
2018-09-30 00:04:36 -0400

    Add and wire up move animations

--- weekly-game-jam-64/Assets/Scripts/Player.cs
+++ weekly-game-jam-64/Assets/Scripts/Player.cs
@@ -8,6 +8,9 @@ public class Player : MonoBehaviour {
     private BoxCollider2D _boxCollider;
     private Animator _animator;
 
+    public float Speed = 0.5f;
+    
+
     void Awake() {
         _boxCollider = GetComponent<BoxCollider2D>();
         _animator = GetComponent<Animator>();
@@ -17,25 +20,26 @@ public class Player : MonoBehaviour {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        _moveDelta = new Vector3(x, y, 0);
-
-        // control sprite direction
-        if (_moveDelta.x > 0) {
-            transform.localScale = Vector3.one;
-        } else if (_moveDelta.x < 0) {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
+        _moveDelta = new Vector3(x, y, 0) * Time.deltaTime * Speed;
 
         RaycastHit2D hit =
             Physics2D.BoxCast(transform.position, _boxCollider.size, 0, _moveDelta,
-                _moveDelta.magnitude * Time.deltaTime,
+                _moveDelta.magnitude,
                 LayerMask.GetMask("Collision", "Actors"));
         if (hit.collider == null) {
-            transform.Translate(_moveDelta * Time.deltaTime);
+            transform.Translate(_moveDelta);
         }
 
         if (_moveDelta.x > 0) {
-            _animator.animation = 
+            _animator.Play("player_walking_right");
+        } else if (_moveDelta.x < 0) {
+            _animator.Play("player_walking_left");
+        } else if (_moveDelta.y < 0) {
+            _animator.Play("player_walking_frontal");
+        } else if (_moveDelta.y > 0) {
+            _animator.Play("player_walking_up");
+        } else {
+            _animator.Play("player_idle_frontal");
         }
     }
 
