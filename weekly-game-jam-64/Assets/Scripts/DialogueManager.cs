16dcefff0482015f6f7e9b6d13325ed3e2442dc0
Manuel Odendahl <wesen@ruinwesen.com>
2018-09-30 10:22:39 -0400

    Add nice dialogue pixel font

---
+++ weekly-game-jam-64/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+    public GameObject DialogueBox;
+    public Text Message;
+    public Text ContinueMessage;
+
+    public bool IsDialogActive = false;
+    
+    void Update() {
+        if (IsDialogActive && Input.GetButtonDown("Submit")) {
+            IsDialogActive = true;
+        }
+    }
+}
