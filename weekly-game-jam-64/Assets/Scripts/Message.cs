f3329791857bb37a8bf657b4c9d8f741442c8d19
Manuel Odendahl <wesen@ruinwesen.com>
2018-10-03 16:18:12 -0400

    Updating dialogue background

--- weekly-game-jam-64/Assets/Scripts/Message.cs
+++ weekly-game-jam-64/Assets/Scripts/Message.cs
@@ -3,22 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Message : MonoBehaviour {
-    private Dialogue _dialogue = new Dialogue();
-
+    private DialogueTrigger _dialogueTrigger;
     private Transform _player;
 
     public void Start() {
         _player = GameObject.FindGameObjectWithTag("Player").transform;
+        _dialogueTrigger = GetComponent<DialogueTrigger>();
     }
 
     public void FillFromMessage(GhostMessage message) {
-        _dialogue.name = message.Name;
-        _dialogue.sentences = message.Message.Split('\n');
+        _dialogueTrigger.dialogue.name = message.Name;
+        _dialogueTrigger.dialogue.sentences = message.Message.Split('\n');
     }
 
     public void Update() {
         if (Vector2.Distance(transform.position, _player.position) < 0.2 && Input.GetKeyDown(KeyCode.E)) {
-            DialogueManager.Instance.StartDialogue(_dialogue);
+            _dialogueTrigger.TriggerDialogue();
         }
     }
 }
--- weekly-game-jam-64/Assets/Scripts/UI/Dialog/NPC_Dialogue_Controller.cs
+++ weekly-game-jam-64/Assets/Scripts/UI/Dialog/NPC_Dialogue_Controller.cs
@@ -10,12 +10,14 @@ public class NPC_Dialogue_Controller : MonoBehaviour {
     private int timeSinceTalk = 0;
     public int talkResetDelay = 1000;
 
-    private void Start() {
+    private void Start()
+    {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         timeSinceTalk = 0;
     }
 
-    private void Update() {
+    private void Update()
+    {
         if (Vector2.Distance(transform.position, player.position) < 0.2 && Input.GetKeyDown(KeyCode.E)) {
             if (hasTalked == false) {
                 dialogueTrig.TriggerDialogue();
@@ -23,13 +25,16 @@ public class NPC_Dialogue_Controller : MonoBehaviour {
                 bubble.SetActive(false);
             }
         } else {
-            if (hasTalked && timeSinceTalk < talkResetDelay && talkResetDelay != -1) {
+            if (hasTalked && timeSinceTalk < talkResetDelay && talkResetDelay != -1)
+            {
                 timeSinceTalk += 1;
-            } else if (hasTalked && timeSinceTalk >= talkResetDelay && talkResetDelay != -1) {
+            }
+            else if (hasTalked && timeSinceTalk >= talkResetDelay && talkResetDelay != -1) {
                 timeSinceTalk = 0;
                 bubble.SetActive(true);
                 hasTalked = false;
             }
         }
+        
     }
 }
