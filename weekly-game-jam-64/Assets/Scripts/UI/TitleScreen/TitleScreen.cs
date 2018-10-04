99544ee338ba49b85c69b0cca266e0f04a4265b3
Manuel Odendahl <wesen@ruinwesen.com>
2018-10-03 21:44:49 -0400

    Add title menu music and selection keys

--- weekly-game-jam-64/Assets/Scripts/UI/TitleScreen/TitleScreen.cs
+++ weekly-game-jam-64/Assets/Scripts/UI/TitleScreen/TitleScreen.cs
@@ -3,15 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Newtonsoft.Json.Bson;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour {
     public List<TitleButton> Buttons;
     public Transform Arrow;
 
     private int _activeIndex = 0;
-
-    void Awake() {
-    }
 
     void Start() {
         _selectEntry(0);
@@ -38,7 +36,20 @@ public class TitleScreen : MonoBehaviour {
             int idx = Math.Min(Buttons.Count - 1, _activeIndex + 1);
             _selectEntry(idx);
         } else if (Input.GetKeyDown(KeyCode.Return)) {
-            Debug.Log("Select action");
+            if (_activeIndex == 0) {
+                PlayGame();
+            } else if (_activeIndex == 1) {
+                QuitGame();
+            }
         }
+    }
+    
+	public void PlayGame() {
+	    SceneManager.LoadScene("TopDownTest");
+	}
+
+    public void QuitGame(){
+        Debug.Log("Quit game. Does not quit in editor, only build");
+        Application.Quit();
     }
 }
